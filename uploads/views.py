7a6109ca73674eff1fa5c3c606a56c064a69317a
detona115 <35955346+detona115@users.noreply.github.com>
2020-07-17 12:57:00 -0400

    Update views.py

--- uploads/views.py
+++ uploads/views.py
@@ -39,35 +39,5 @@ class FotosPageView(ListView):
         else:
             return UploadedFilesThumb.objects.all()
     
-    # response = HttpResponse(content_type='application/force-download')
-    # response['Content-Disposition'] = f'attachment; filename="image"'
-
-    # def get(self,request,file_name):
-    #     response = HttpResponse(content_type='application/force-download')
-    #     response['Content-Disposition'] = 'attachment; filename=%s' % smart_str(file_name)
-    #     response['X-Sendfile'] = smart_str(path_to_file)
-        # file_path = settings.MEDIA_ROOT +'/'+ file_name
-        # file_wrapper = FileWrapper(open(file_path,'rb'))
-        # file_mimetype = mimetypes.guess_type(file_path)
-        # response = HttpResponse(file_wrapper, content_type=file_mimetype )
-        # response['X-Sendfile'] = file_path
-        # response['Content-Length'] = os.stat(file_path).st_size
-        # response['Content-Disposition'] = 'attachment; filename=%s/' % str(file_name) 
-        # return response
     
-    # def get(self, request, *args, **kwargs):
 
-    #     response = HttpResponse(content_type='application/force-download')
-    #     response['Content-Disposition'] = f'attachment; filename="image"'
-    #     # response['X-Sendfile'] = smart_str(path_to_file)
-
-    #     return super().get(request, *args, **kwargs)
-
-
-
-# def FotosDownloaded(request):
-
-#     response = HttpResponse(content_type='application/force-download')
-#     response['Content-Disposition'] = f'attachment; filename="image.jpg"'
-
-#     return response
